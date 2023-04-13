db1afdfcf93308785cc6ba7ee72a22c32557cbbc
Ashruti41 <98145165+Ashruti41@users.noreply.github.com>
2023-04-13 12:26:32 +0530

    Update shinc1.c

--- shinc1.c
+++ shinc1.c
@@ -1,4 +1,4 @@
-/* gst-launch-1.0 filesrc location=/Users/siyao/Movies/why_I_left_China_for_Good.mp4 ! qtdemux name=demux  demux.audio_0 ! queue ! decodebin ! audioconvert ! audioresample ! autoaudiosink  demux.video_0 ! queue ! decodebin ! videoconvert ! videoscale ! autovideosink */
+/* gst-launch-1.0 filesrc location=/home/ashruti/test.mp4 ! qtdemux name=demux demux.audio_0 ! queue ! decodebin ! audioconvert ! audioresample ! autoaudiosink demux.video_0 ! queue ! decodebin ! videoconvert ! videoscale ! autovideosink */
 
 
 #include <gst/gst.h>
